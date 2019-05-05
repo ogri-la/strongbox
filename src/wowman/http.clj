6d6f4f2ebbe4db64a5f5a93a8612fd751c2a6d41
Torkus <48141663+ogri-la@users.noreply.github.com>
2019-05-05 12:38:29 +0930

    http/download now uses modtime on file to determine freshness rather than the daily cache directory previously used. expiry is still 24hrs

--- src/wowman/http.clj
+++ src/wowman/http.clj
@@ -6,6 +6,9 @@
    [clojure.string]
    [clojure.java.io]
    [clojure.spec.alpha :as s]
+   [clj-time
+    [core :as t]
+    [coerce :as coerce-time]]
    [orchestra.core :refer [defn-spec]]
    [orchestra.spec.test :as st]
    [taoensso.timbre :as log :refer [debug info warn error spy]]
@@ -67,16 +70,32 @@
         wowman-useragent (format "Wowman/%s (https://github.com/ogri-la/wowman)" wowman-version)]
     {"http.useragent" (if use-anon-useragent? anon-useragent wowman-useragent)}))
 
+(defn fresh-cache-file-exists?
+  "returns `true` if the last modification time on given file is before the expiry date of +N hours"
+  [output-file]
+  (when (and output-file (fs/exists? output-file))
+    (let [modtime (coerce-time/from-long (fs/mod-time output-file))
+          now (t/now)
+          expiry-offset (t/hours 24)
+          expiry-date (t/plus modtime expiry-offset)
+          expired? (t/before? expiry-date now)]
+      (debug (format "modtime %s; expiry-offset %s; expiry-date %s; now %s; expired? %s"
+                     modtime expiry-offset expiry-date now expired?))
+      (not expired?))))
+
 (defn-spec -download (s/or :file ::sp/extant-file, :raw ::sp/http-resp, :error ::sp/http-error)
+  "if writing to a file is possible, then the output file will be returned, otherwise the raw http response
+   writing to a file is possible when caching is available or an `output-file` is given to us."
   [uri ::sp/uri, output-file (s/nilable ::sp/file), message (s/nilable ::sp/short-string), extra-params map?]
   (let [cache? (not (nil? *cache*))
-        ext (-> uri fs/split-ext second (or ".?"))
+        ext (-> uri fs/split-ext second (or ".html")) ;; *probably* html, we don't particularly care
         encoded-path (-> uri .getBytes b64/encode String. (str ext))
         alt-output-file (when cache?
                           (utils/join (:cache-dir *cache*) encoded-path)) ;; "/path/to/cache/aHR0[...]cHM6=.html"
-        output-file (or output-file alt-output-file) ;; `output-file` may still be `nil` after this.
+        output-file (or output-file alt-output-file) ;; `output-file` may still be nil after this!
 
-        etag-key (when cache? (-> output-file fs/base-name (str ".etag")))]
+        etag-key (when cache? (-> output-file fs/base-name (str ".etag")))
+        streaming-response? (-> extra-params :as (= :stream))]
 
     ;; ensures orphaned .etag files don't prevent download of missing files
     (when (and cache?
@@ -85,61 +104,63 @@
       (warn "orphaned .etag found:" etag-key)
       ((:set-etag *cache*) etag-key)) ;; dissoc etag from db
 
-    (try
-      (info (or message (format "downloading %s to %s" (fs/base-name uri) output-file)))
-      (client/with-additional-middleware [client/wrap-lower-case-headers (etag-middleware etag-key)]
-        (let [params {:redirect-strategy curse-crap-redirect-strategy}
-              use-anon-useragent? false
-              params (merge params (user-agent use-anon-useragent?) extra-params)
-              _ (debug "requesting" uri "with params" params)
-              resp (client/get uri params)
-              _ (debug "response status" (:status resp))
+    ;; use the file on disk if it's not too old ...
+    (if (fresh-cache-file-exists? output-file)
+      output-file
 
-              not-modified (= 304 (:status resp)) ;; 304 is "not modified" (local file is still fresh). only happens when caching
-              modified (not not-modified)]
+      ;; ... otherwise, we must sing and dance
+      (try
+        (info (or message (format "downloading %s to %s" (fs/base-name uri) output-file)))
+        (client/with-additional-middleware [client/wrap-lower-case-headers (etag-middleware etag-key)]
+          (let [params {:redirect-strategy curse-crap-redirect-strategy}
+                use-anon-useragent? false
+                params (merge params (user-agent use-anon-useragent?) extra-params)
+                _ (debug "requesting" uri "with params" params)
+                resp (client/get uri params)
+                _ (debug "response status" (:status resp))
 
-          (when not-modified
-            (debug "not modified, contents will be read from cache:" output-file))
+                not-modified (= 304 (:status resp)) ;; 304 is "not modified" (local file is still fresh). only happens when caching
+                modified (not not-modified)]
 
-          (cond
-            ;; remote data has changed, write body to disk
-            (and output-file modified) (do
-                                         (clojure.java.io/copy (:body resp) (java.io.File. output-file)) ;; doesn't .close on streams
-                                         (if (-> params :as (= :stream))
-                                           (do
+            (when not-modified
+              (debug "not modified, contents will be read from cache:" output-file))
+
+            (cond
+              ;; remote data has changed, write body to disk
+              (and output-file modified) (do
+                                           (clojure.java.io/copy (:body resp) (java.io.File. output-file)) ;; doesn't .close on streams
+                                           (when streaming-response?
                                              ;; stream request responses get written to a file, the stream closed and the path to the output file returned
-                                             (-> resp :body .close) ;; not all requests are streams with a :body that needs to be closed
-                                             (assoc resp :body output-file))
-                                           ;; regular request responses get the :body as-is
-                                           resp))
+                                             (-> resp :body .close)) ;; not all requests are streams with a :body that needs to be closed
+                                           output-file)
 
-            ;; remote data has not changed, :body is nil, replace it with path to file
-            (and output-file not-modified) (if (-> params :as (= :stream))
-                                             ;; stream request response already written to file
-                                             (assoc resp :body output-file)
-                                             (assoc resp :body (slurp output-file)))
+              ;; remote data has not changed, :body is nil, replace it with path to file
+              (and output-file not-modified) (do
+                                               (fs/touch output-file) ;; update modtime
+                                               output-file)
 
-            ;; (and (not output-file) modified) ;; standard http request of textual content with caching turned off. return the resp as-is
-            ;; (and (not output-file) not-modified) ;; not possible. if-none-match header only added if etag-key found in etag-db. etag-key is derived from output-file
+              ;; (and (not output-file) modified) ;; standard http request of textual content with caching turned off. return the resp as-is
+              ;; (and (not output-file) not-modified) ;; not possible. if-none-match header only added if etag-key found in etag-db. etag-key is derived from output-file
 
-            :else resp)))
+              :else resp)))
 
-      (catch Exception ex
-        (if (-> ex ex-data :status)
-          ;; "Note that the connection to the server will NOT be closed until the stream has been read"
-          ;;  - https://github.com/dakrone/clj-http
-          (let [_ (some-> ex ex-data :body .close)
-                http-error (select-keys (ex-data ex) [:reason-phrase :status])]
-            (error (format "failed to download file '%s': %s (HTTP %s)"
-                           uri (:reason-phrase http-error) (:status http-error)))
-            http-error)
+        (catch Exception ex
+          (if (-> ex ex-data :status)
+            ;; "Note that the connection to the server will NOT be closed until the stream has been read"
+            ;;  - https://github.com/dakrone/clj-http
+            (let [_ (some-> ex ex-data :body .close)
+                  http-error (select-keys (ex-data ex) [:reason-phrase :status])]
+              (error (format "failed to download file '%s': %s (HTTP %s)"
+                             uri (:reason-phrase http-error) (:status http-error)))
+              http-error)
 
-          ;; unhandled non-http exception
-          (throw ex))))))
+            ;; unhandled non-http exception
+            (throw ex)))))))
 
-(defn-spec http-error? boolean?
-  [http-resp ::sp/http-resp]
-  (<= 400 (:status http-resp)))
+(defn http-error?
+  [http-resp]
+  (and (map? http-resp)
+       (<= 400 (:status http-resp))))
 
 ;;(defn-spec download (s/or :ok ::sp/http-resp, :error ::sp/http-error)
 ;;  [uri ::sp/uri, message ::sp/short-string]
@@ -147,9 +168,10 @@
   [uri & {:keys [message]}]
   (let [output-file nil
         resp (-download uri output-file message {})]
-    (if-not (http-error? resp)
-      (:body resp)
-      resp)))
+    (cond
+      (http-error? resp) resp ;; error http response
+      (map? resp) (:body resp) ;; regular http response + caching disabled
+      (fs/file? resp) (slurp resp)))) ;; file on disk + caching enabled
 
 ;;(defn-spec download-file (s/or :file ::sp/extant-file, :error ::sp/http-error)
 ;;  [uri ::sp/uri, output-file ::sp/file, & {:keys [message]}]
