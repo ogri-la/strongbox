96df80e788355f1f3eac478aea747bffe525e617
Torkus <48141663+ogri-la@users.noreply.github.com>
2019-05-14 19:35:12 +0930

    version bumps

--- project.clj
+++ project.clj
@@ -6,16 +6,16 @@
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/spec.alpha "0.2.176"]
-                 [org.clojure/tools.cli "0.4.1"] ;; cli arg parsing
+                 [org.clojure/tools.cli "0.4.2"] ;; cli arg parsing
                  [org.clojure/tools.namespace "0.2.11"] ;; reload code
                  [org.clojure/data.codec "0.1.1"] ;; base64 encoding
                  [org.clojure/data.json "0.2.6"] ;; better json decoding
                  [orchestra "2018.12.06-2"] ;; improved clojure.spec instrumentation
                  ;; see lein deps :tree
-                 [metosin/spec-tools "0.9.0" :exclusions [com.fasterxml.jackson.core/jackson-core]] ;; more improvements to clojure.spec handling
+                 [metosin/spec-tools "0.9.2" :exclusions [com.fasterxml.jackson.core/jackson-core]] ;; more improvements to clojure.spec handling
                  [com.taoensso/timbre "4.10.0"] ;; logging
                  [enlive "1.1.6"] ;; html parsing
-                 [clj-http "3.8.0"] ;; better http slurping
+                 [clj-http "3.10.0"] ;; better http slurping
                  [cheshire "5.8.1"] ;; nicer json serialisation (indents)
                  [seesaw "1.5.0"] ;; swing
                  [me.raynes/fs "1.4.6"] ;; file system wrangling
