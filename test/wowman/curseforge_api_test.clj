492941eff0f5288df2bf0f435f7edb51df75dc24
Torkus <48141663+ogri-la@users.noreply.github.com>
2019-08-31 11:12:26 +0930

    added test for addon expansion when selected game track doesn't match

--- test/wowman/curseforge_api_test.clj
+++ test/wowman/curseforge_api_test.clj
@@ -8,7 +8,7 @@
     [test-helper :as helper :refer [fixture-path temp-path]]]))
 
 (deftest expand-summary
-  (testing "curseforge-api addon expansion"
+  (testing "simple addon expansion, ideal conditions"
     (let [api-results (slurp (fixture-path "curseforge-api-addon--everyaddon.json"))
           fake-routes {"https://addons-ecs.forgesvc.net/api/v2/addon/1"
                        {:get (fn [req] {:status 200 :body api-results})}}
@@ -31,9 +31,31 @@
           expected (merge addon-summary {:download-uri "https://edge.forgecdn.net/files/1/1/EveryAddon.zip"
                                          :version "v8.2.0-v1.13.2-7135.139"
                                          :interface-version 11300 ;; "1.13.2" => 11300
-                                         })]
+                                         })
+          game-track "retail"]
       (with-fake-routes-in-isolation fake-routes
-        (is (= expected (curseforge-api/expand-summary addon-summary "retail")))))))
+        (is (= expected (curseforge-api/expand-summary addon-summary game-track))))))
+
+  (testing "addon expansion when selected game track doesn't match anything available in releases"
+    (let [api-results (slurp (fixture-path "curseforge-api-addon--everyaddon.json"))
+          fake-routes {"https://addons-ecs.forgesvc.net/api/v2/addon/1"
+                       {:get (fn [req] {:status 200 :body api-results})}}
+          addon-summary {:created-date "2010-05-07T18:48:16Z",
+                         :description "Does what no other addon does, slightly differently",
+                         :category-list ["Bags & Inventory"],
+                         :updated-date "2019-06-26T01:21:39Z",
+                         :age "new",
+                         :name "everyaddon",
+                         :source "curseforge",
+                         :alt-name "everyaddon",
+                         :label "EveryAddon",
+                         :download-count 3000000,
+                         :source-id 1,
+                         :uri "https://www.curseforge.com/wow/addons/everyaddon"}
+          game-track "classic"
+          expected nil]
+      (with-fake-routes-in-isolation fake-routes
+        (is (= expected (curseforge-api/expand-summary addon-summary game-track)))))))          
 
 (deftest latest-versions-by-game-track
   (testing "data in :latestFiles is filtered and grouped correctly"
