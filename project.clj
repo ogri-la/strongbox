fbb0374a6b3cfba2e34472fb5b01a4bf3c3fc8c0
Torkus <48141663+ogri-la@users.noreply.github.com>
2020-04-22 00:09:08 +0930

    0.12.4 prep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ogri-la/wowman "0.12.4-unreleased"
+(defproject ogri-la/wowman "0.12.4"
   :description "World Of Warcraft Addon Manager"
   :url "https://github.com/ogri-la/wowman"
   :license {:name "GNU Affero General Public License (AGPL)"
