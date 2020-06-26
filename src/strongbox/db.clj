eb35398869c3ecca9edc3c3b01c8bb11bfbfbdd1
Torkus <48141663+ogri-la@users.noreply.github.com>
2020-06-26 22:05:23 +0930

    random sample frequency is increased when catalogue size is smaller

--- src/strongbox/db.clj
+++ src/strongbox/db.clj
@@ -100,16 +100,17 @@
   label-matching matches from the beginning of the label.
   description-matching matches any substring within description"
   [db :addon/summary-list, uin (s/nilable string?), cap int?]
-  (if (nil? uin)
-    (take cap (random-sample 0.005 db))
-    (let [label-regex (re-pattern (str "(?i)^" uin ".*"))
-          desc-regex (re-pattern (str "(?i).*" uin ".*"))
-          ;; a little slow and naive, but ok for now
-          xf (filter (fn [row]
-                       (or
-                        (re-find label-regex (:label row))
-                        (re-find desc-regex (get row :description "")))))]
-      (into [] (comp xf (take cap)) db))))
+  (let [pct (->> db count (max 1) (/ 100) (* 0.6))]
+    (if (nil? uin)
+      (take cap (random-sample pct db))
+      (let [label-regex (re-pattern (str "(?i)^" uin ".*"))
+            desc-regex (re-pattern (str "(?i).*" uin ".*"))
+            ;; a little slow and naive, but ok for now
+            xf (filter (fn [row]
+                         (or
+                          (re-find label-regex (:label row))
+                          (re-find desc-regex (get row :description "")))))]
+        (into [] (comp xf (take cap)) db)))))
 
 ;; not specced because the results and argument lists may vary greatly
 (defn stored-query
