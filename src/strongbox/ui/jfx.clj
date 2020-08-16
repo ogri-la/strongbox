a2219455589c236c71e9a7275ca2c03bf94df9ea
Torkus <48141663+ogri-la@users.noreply.github.com>
2020-08-16 13:15:39 +0930

    jfx, gui is now re-rendered on state change

--- src/strongbox/ui/jfx.clj
+++ src/strongbox/ui/jfx.clj
@@ -4,25 +4,23 @@
    [cljfx.api :as fx]
    [strongbox
     [core :as core]
-    [utils :as utils :refer [items]]]
-   ))
-
+    [utils :as utils :refer [items]]]))
 
 (def INSTALLED-TAB 0)
 (def SEARCH-TAB 1)
 
 (defn menu-item
-  [label handler & [opts]]
+  [label handler & [_]]
   {:fx/type :menu-item
    :text (str label)})
-
 
 (defn build-catalogue-menu
   []
   [])
 
 (defn menu
-  [label items & [opts]]
+  [label items & [_]]
+
   {:fx/type :menu
    :text label
    :items items})
@@ -38,7 +36,7 @@
     (f)))
 
 (defn donothing
-  [[& args]]
+  [[& _]]
   nil)
 
 (def separator {:fx/type fx/ext-instance-factory
@@ -56,15 +54,14 @@
   nil)
 
 (defn menu-bar
-  []
+  [state]
   (let [exit-handler (fn [ev]
                        nil)
-        
+
         file-menu [(menu-item "New addon directory" (async-handler wow-dir-picker) {:key "menu N" :mnemonic "n"})
                    (menu-item "Remove addon directory" (async-handler core/remove-addon-dir!))
                    separator
-                   (menu-item "Exit" exit-handler {:key "menu Q" :mnemonic "x"})
-                   ]
+                   (menu-item "Exit" exit-handler {:key "menu Q" :mnemonic "x"})]
 
         view-menu [(menu-item "Refresh" (async-handler core/refresh) {:key "F5"})
                    separator
@@ -74,8 +71,7 @@
 
         catalogue-menu (into (build-catalogue-menu)
                              [separator
-                              (menu-item "Refresh user catalogue" (async-handler core/refresh-user-catalogue))
-                              ])
+                              (menu-item "Refresh user catalogue" (async-handler core/refresh-user-catalogue))])
 
         addon-menu [(menu-item "Update all" (async-handler core/install-update-all) {:key "menu U" :mnemonic "u"})
                     (menu-item "Re-install all" (async-handler core/re-install-all))]
@@ -96,9 +92,7 @@
                     (menu-item "Delete .wowman.json files" (async-handler (comp core/refresh core/delete-wowman-json-files!)))
                     (menu-item "Delete .strongbox.json files" (async-handler (comp core/refresh core/delete-strongbox-json-files!)))]
 
-        help-menu [(menu-item "About strongbox" (handler about-strongbox-dialog))]
-
-        ]
+        help-menu [(menu-item "About strongbox" (handler about-strongbox-dialog))]]
 
     {:fx/type :menu-bar
      :id "main-menu"
@@ -108,40 +102,37 @@
              (menu "Addons" addon-menu {:mnemonic "A"})
              (menu "Import/Export" impexp-menu {:mnemonic "i"})
              (menu "Cache" cache-menu)
-             (menu "Help" help-menu)
-             ]}))
+             (menu "Help" help-menu)]}))
 
 ;; tabber
 
+
 (defn installed-addons-menu-bar
-  []
+  [state]
   (let [update-all-button {:fx/type :button
                            :text "Update all"}
         wow-dir-dropdown {:fx/type :combo-box
                           :items ["/home/torkus/path/to/wine/prefix/World of Warcraft/_retail_/Interface/Addons/"]}
-        
+
         game-track-dropdown {:fx/type :combo-box
-                             :items ["retail" "classic"]}
-        ]
+                             :items ["retail" "classic"]}]
     {:fx/type :h-box
      :padding 10
      :spacing 10
      :children [update-all-button
                 wow-dir-dropdown
-                game-track-dropdown
-                ]}))
+                game-track-dropdown]}))
 
 (defn mkcol [label]
   {:fx/type :table-column
    :text label})
 
 (defn installed-addons-table
-  []
+  [state]
   {:fx/type :v-box
    :children [{:fx/type :table-view
                :columns (mapv mkcol ["source" "name" "description" "installed" "available" "WoW"])
                :items []}]})
-
 
 (defn app-notice-logger
   []
@@ -150,51 +141,51 @@
                :columns (mapv mkcol ["level" "message"])}]})
 
 (defn installed-addons-pane
-  []
+  [state]
   {:fx/type :v-box
-   :children [(installed-addons-menu-bar)
+   :children [{:fx/type installed-addons-menu-bar}
               {:fx/type :split-pane
                :orientation :vertical
-               :divider-positions [0.5]
-               :items [(installed-addons-table)
-                       (app-notice-logger)
-                       ]}
-              ]})
+               :divider-positions [0.7]
+               :items [{:fx/type installed-addons-table}
+                       (app-notice-logger)]}]})
+
 (defn tabber
-  []
+  [state]
   {:fx/type :tab-pane
    :tabs [{:fx/type :tab
            :text "installed"
            :closable false
-           :content (installed-addons-pane)}
+           :content {:fx/type installed-addons-pane}}
           {:fx/type :tab
            :text "search"
            :closable false}]})
 
-
 ;;
 
 (defn root
-  []
-  {:fx/type :v-box
-   :children [(menu-bar)
-              (tabber)
-              ]})
+  [state]
+  (info "rendering root" state)
+  {:fx/type :stage
+   :showing true
+   :title "strongbox"
+   :width 1024
+   :height 768
+   :scene {:fx/type :scene
+           :root {:fx/type :v-box
+                  :children [{:fx/type menu-bar}
+                             {:fx/type tabber}]}}})
 
 (defn start
   []
   (info "starting gui")
-  (let [ui {:fx/type :stage
-            :showing true
-            :title "strongbox"
-            :width 1024
-            :height 768
-            :scene {:fx/type :scene
-                    :root (root)}}]
-    
-    (info "ui" ui)
-    (fx/on-fx-thread
-     (fx/create-component ui))))
+  (let [middleware (fn [state]
+                     ;; might want to do a select-keys here,
+                     ;; watch just certain bits of the state
+                     (-> state :gui2 (assoc :fx/type root)))
+        renderer (fx/create-renderer
+                  :middleware (fx/wrap-map-desc middleware))]
+    (fx/mount-renderer core/state renderer)))
 
 (defn stop
   []
