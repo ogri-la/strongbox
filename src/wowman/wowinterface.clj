7ac5f6b539508d866a6f6a07d40d33907b91008c
Torkus <48141663+ogri-la@users.noreply.github.com>
2020-09-19 21:57:00 +0930

    wowman, wowi, backported datetime format bug

--- src/wowman/wowinterface.clj
+++ src/wowman/wowinterface.clj
@@ -1,6 +1,7 @@
 (ns wowman.wowinterface
   (:require
-   [clojure.string :refer [trim]]
+   [orchestra.core :refer [defn-spec]]
+   [clojure.string :refer [trim lower-case upper-case]]
    [clojure.set]
    [slugify.core :refer [slugify]]
    [orchestra.spec.test :as st]
@@ -21,16 +22,28 @@
    "cat109.html" "Info, Plug-in Bars"
    "cat158.html" "Classic - General"})
 
-(defn format-wowinterface-dt
+(defn-spec -format-wowinterface-dt string?
   "formats a shitty US-style m/d/y date with a shitty 12 hour time component and no timezone
   into a glorious RFC3399 formatted UTC string."
-  [dt]
+  [dt string?]
   (let [;; https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
         dt (java-time/local-date-time "MM-dd-yy hh:mm a" dt) ;; "09-07-18 01:27 PM" => obj with no tz
         ;; no tz info available on site, assume utc
         dt-utc (java-time/zoned-date-time dt "UTC") ;; obj with no tz => utc obj
         fmt (get java-time.format/predefined-formatters "iso-offset-date-time")]
     (java-time/format fmt dt-utc)))
+
+(defn-spec format-wowinterface-dt string?
+  "formats a shitty US-style m/d/y date with a shitty 12 hour time component and no timezone
+  into a glorious RFC3399 formatted UTC string."
+  [dt string?]
+  (try
+    (-format-wowinterface-dt (lower-case dt)) ;; lowercase (java 11) first
+    (catch Exception e ;; DateTimeParseException *isn't* being thrown here
+      ;; because of some locale bs, datetime formatting is case sensitive in java 11 but not java 8
+      ;; and only in non-US locales. This is why it passes tests in CI but not locally.
+      ;; -- https://stackoverflow.com/questions/38250379/java8-datetimeformatter-am-pm
+      (-format-wowinterface-dt (upper-case dt))))) ;; upper-case (java 8) is what is returned by wowi. 
 
 (defn scrape-category-group-page
   [category-page] ;; => "cat23.html"
