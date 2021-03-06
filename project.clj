a1327456be4e6415ba0ed6617a472f03453edbf4
Torkus <48141663+ogri-la@users.noreply.github.com>
2021-03-07 00:22:46 +1030

    bumps cljfx deps

--- project.clj
+++ project.clj
@@ -23,21 +23,21 @@
                  [envvar "1.1.0"] ;; environment variable wrangling
                  [gui-diff "0.6.7"] ;; pops up a graphical diff for test results
                  [com.taoensso/tufte "2.1.0"]
-                 [cljfx "1.7.8" :exclusions [org.openjfx/javafx-web
+                 [cljfx "1.7.13" :exclusions [org.openjfx/javafx-web
                                              org.openjfx/javafx-media]]
                  [cljfx/css "1.1.0"]
 
-                 [org.openjfx/javafx-base "14"]
-                 [org.openjfx/javafx-base "14" :classifier "linux"]
-                 [org.openjfx/javafx-base "14" :classifier "mac"]
+                 [org.openjfx/javafx-base "15.0.1"]
+                 [org.openjfx/javafx-base "15.0.1" :classifier "linux"]
+                 [org.openjfx/javafx-base "15.0.1" :classifier "mac"]
 
-                 [org.openjfx/javafx-controls "14"]
-                 [org.openjfx/javafx-controls "14" :classifier "linux"]
-                 [org.openjfx/javafx-controls "14" :classifier "mac"]
+                 [org.openjfx/javafx-controls "15.0.1"]
+                 [org.openjfx/javafx-controls "15.0.1" :classifier "linux"]
+                 [org.openjfx/javafx-controls "15.0.1" :classifier "mac"]
 
-                 [org.openjfx/javafx-graphics "14"]
-                 [org.openjfx/javafx-graphics "14" :classifier "linux"]
-                 [org.openjfx/javafx-graphics "14" :classifier "mac"]
+                 [org.openjfx/javafx-graphics "15.0.1"]
+                 [org.openjfx/javafx-graphics "15.0.1" :classifier "linux"]
+                 [org.openjfx/javafx-graphics "15.0.1" :classifier "mac"]
 
                  ;; remember to update the LICENCE.txt
                  ;; remember to update pom file (`lein pom`)
