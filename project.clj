ae5a4ab8e201724a938e6f118e6fc6f9c761caf1
Torkus <48141663+ogri-la@users.noreply.github.com>
2021-12-12 13:06:01 +0000

    4.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ogri-la/strongbox "4.8.0-unreleased"
+(defproject ogri-la/strongbox "4.8.0"
   :description "World Of Warcraft Addon Manager"
   :url "https://github.com/ogri-la/strongbox"
   :license {:name "GNU Affero General Public License (AGPL)"
